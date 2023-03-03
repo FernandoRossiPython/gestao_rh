d479e4078348256b31c3cc8ea3741fee35e8cd43
FernandoRossiPythonm <fernando.cgr@gmail.com>
2023-03-03 14:50:51 -0300

    Criando a Apps Empresas

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'apps.empresas',
+   ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
